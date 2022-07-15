c7b13f7c0cc7f43d75af0bde4cb142492208df77
Amanda Remo <amandaremo@ip-192-168-86-42.ec2.internal>
2022-07-14 20:12:06 -0400

    finished commit

--- public/index.js
+++ public/index.js
@@ -1,9 +1,18 @@
 function main(){
+    const button = document.createElement("button")
+    button.textContent = "reset";
+
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
-
-    let countValue = 0;
+    const resetButton = document.querySelector('#reset-button');
+    const url = "http://localhost:9001/counter";
+    const counterRes = await fetch(url);
+    const {value} = await counterRes.json();
+    countValue = value;
+    console.log({countValue});
+    
+    // let countValue = 0;
 
     function increment(){
         countValue++;
@@ -14,9 +23,13 @@ function main(){
         countValue--;
         countContainer.textContent = countValue;
     }
+    function reset() {
+        main();
+    }
 
     incrementButton.addEventListener('click', increment);
     decrementButton.addEventListener('click', decrement);
+    resetButton.addEventListener('click', reset);
     countContainer.textContent = countValue;
 }
 main()
